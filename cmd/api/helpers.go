439f04cebdb8ab3bdd30df042e39944e84cce91e
Stephanus Nugraha <mail@stephanusnugraha.com>
2022-11-09 10:20:19 +0700

    add func handleSubmission and authenticate

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -54,7 +54,7 @@ func (app *Config) writeJSON(w http.ResponseWriter, status int, data any, header
 	return nil
 }
 
-func (app *Config) errJSON(w http.ResponseWriter, err error, status ...int) error {
+func (app *Config) errorJSON(w http.ResponseWriter, err error, status ...int) error {
 	statusCode := http.StatusBadRequest
 
 	if len(status) > 0 {

80f24095ae56f5cad4f762d1f6f3ddcec64cc5ea
Stephanus Nugraha <mail@stephanusnugraha.com>
2022-11-16 13:43:41 +0700

    update emitter file; add handler for log event rabbitmq

--- event/emitter.go
+++ event/emitter.go
@@ -9,7 +9,7 @@ type Emitter struct {
 	connection *amqp.Connection
 }
 
-func (e Emitter) setup() error {
+func (e *Emitter) setup() error {
 	channel, err := e.connection.Channel()
 	if err != nil {
 		return err
@@ -19,7 +19,7 @@ func (e Emitter) setup() error {
 	return declareExchange(channel)
 }
 
-func (e Emitter) push(event string, severity string) error {
+func (e *Emitter) Push(event string, severity string) error {
 	channel, err := e.connection.Channel()
 	if err != nil {
 		return err
